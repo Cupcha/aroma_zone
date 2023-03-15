17f1e77f4ed14029328ade39f397ea45cc0e7fe7
Cupcha <cupillard.charlotte@gmail.com>
2023-03-15 10:29:11 +0100

    stars

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -32,7 +32,7 @@
 </svelte:head>
 
 <body class="home">
-			<header class="fixed w-full z-0">
+			<header class="fixed w-full z-1">
 				<div id="section_brands" class="w-full items-center md:font-medium">
 					<div id="shop" class="box-border flex absolute justify-center left-0 top-0 h-12 w-1/2 bg-purplebg">
 						<a href="http://localhost:5173" class="flex items-center gap-2 text-center">
@@ -116,7 +116,7 @@
 					
 				</div>
 		</header>
-		<section id="main z-1">
+		<section id="main z-0">
 			<div class="pt-28">
 				<section>
 					<a class="slide" href="/product/huiles-essentiels/lavande">
@@ -126,7 +126,7 @@
 					</a>
 				</section>
 			</div>
-			<section class="testimonials bg-browntestimonial md:font-medium max-w-full py-16 sm:py-20 sm:px-6 text-browntext" id="testimonials">
+			<section class="testimonials bg-browntestimonial md:font-medium max-w-full py-16 sm:py-20 sm:px-6 text-browntext z-0" id="testimonials">
 				<div class="title text-center">
 					<h2 class="text-title uppercase font-bold text-4xl mb-2">Que Disent Nos Clients</h2>
 					<p>95% de nos clients sont satisfaits !</p>
@@ -142,7 +142,7 @@
 								<svg aria-hidden="true" class="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Fifth star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
 							</div>
 						</div>
-						<div class="text">
+						<div class="text align-middle">
 							<p>La caverne d'Ali Baba si Ali Baba était homme à faire ses cosmétiques maison.</p>
 							<h3 class="mt-4">Aude S.</h3>
 						</div>
