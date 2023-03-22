d0f13fd808b541a57fc8e436376fcd91f8a7a31f
Cupcha <115945494+Cupcha@users.noreply.github.com>
2023-03-23 00:02:06 +0100

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,16 +2,31 @@ const config = {
   content: ["./src/**/*.{html,js,svelte,ts}"],
 
   theme: {
+    fontFamily: {
+       sans: ['Karla', 'sans-serif']
+     },
     extend: {
       colors: {
-        purplebg: "#DECAE1",
+        purplebg: "#D8C3E3",
         purpletext: "#825C88",
-        brownbg: "#ADA098",
+        brownbg: "#D2C9C0",
         browntext:"#8A766A",
         brownmobile: "#D2BA9A",
         browntestimonial: "#F7EFE4",
-      }
-    },
+      },
+      padding: {
+        'custom-top': '1040px 0 0 0', // top right bottom left
+      },
+      fontFamily: {
+        'karla': ['Karla', 'sans-serif'],
+      },
+      // You may need to adjust the font weights based on the font files you downloaded
+      fontWeight: {
+        'regular': 400,
+        'medium': 500,
+        'bold': 700,
+      },
+    }
   },
 
   plugins: [],
