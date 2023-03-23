c60f19128d42fd60c3546b7a4ca54b94c8ee84bd
auroremonnais <115981517+auroremonnais@users.noreply.github.com>
2023-03-23 16:35:18 +0100

    small screen 2 columns grid

--- src/routes/liste_maison/+page.svelte
+++ src/routes/liste_maison/+page.svelte
@@ -102,7 +102,7 @@
 			
 			<product-list class="flex text-browntext text-center">
 				
-				<div class="max-w-2xl mx-auto px-4 py-8 lg:max-w-7xl grid grid-cols-1 gap-y-10 gap-x-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:h-auto">
+				<div class="max-w-2xl mx-auto px-4 py-8 lg:max-w-7xl grid grid-cols-2 gap-y-10 gap-x-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:h-auto">
 					
 					<div class="bg-white shadow-lg rounded-lg">
 						<a href="/maison1">
